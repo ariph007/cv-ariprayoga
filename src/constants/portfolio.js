509492db3644f5e5478897810fbd36dac4698cf5
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-11-24 17:55:31 +0700

    edit package json

--- src/constants/portfolio.js
+++ src/constants/portfolio.js
@@ -1,38 +1,38 @@
 const portfolio = [
   {
     name: 'SmartPOS',
-    imgUrl: '/src/assets/8.png',
+    imgUrl: '/8.png',
     description:
       'Point Of Sales system build, with frontend build with ReactJS, Tailwindcss, React Toastify,' +
       ' AOS, React Hook Form and backend with NodeJS, Express JS, Sequelize, Express Validator, Bcrypt, Multer,Mysql, and JWT',
   },
   {
     name: 'SMT School',
-    imgUrl: '/src/assets/1.jpg',
+    imgUrl: '/1.jpg',
     description:
       'Company Profile for SMT School Batam, Build with wordpress, Office project in PT Garuda Kita',
   },
   {
     name: 'Gomax Equipment',
-    imgUrl: '/src/assets/2.jpg',
+    imgUrl: '/2.jpg',
     description:
       'Online Store for Gomax Equipment.Its a kitchen distributor in Batam. Build with wordpres and woocommerce, Office project in PT Garuda Kita',
   },
   {
     name: 'SMK Pariwisata Adimulia',
-    imgUrl: '/src/assets/3.jpg',
+    imgUrl: '/3.jpg',
     description:
       'Company Profile for SMK Pariwisata Adimulia Batam, Build with wordpress, Office project in PT Garuda Kita',
   },
   {
     name: 'Dapur Sehat Batam',
-    imgUrl: '/src/assets/4.jpg',
+    imgUrl: '/4.jpg',
     description:
       'Company Profile for Dapur Sehat Batam, Build with wordpress, Office project in PT Garuda Kita',
   },
   {
     name: 'Arnold Kitchen',
-    imgUrl: '/src/assets/5.jpg',
+    imgUrl: '/5.jpg',
     description:
       'Company Profile for Arnold Kitchen, Build with wordpress, Office project in PT Garuda Kita',
   },
