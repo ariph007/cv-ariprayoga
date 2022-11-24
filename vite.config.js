572ae5bb8ae9bf2c2bb37bc9e1dfaea9131f44d5
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-11-24 17:14:09 +0700

    Edit title

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cv2023/',
   plugins: [react()],
   commonjsOptions: {
     esmExternals: true,
